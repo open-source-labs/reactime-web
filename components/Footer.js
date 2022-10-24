ce4875645e4aac1b2a6796184ddc17e3f1231812
louislam <louislam7229@gmail.com>
2022-10-24 09:36:00 -0700

    added components and link

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Copyright 2022 Reactime</div>;
+};
+
+export default Footer;
---
+++ pages/info/index.js
@@ -0,0 +1,9 @@
+const Faq = () => {
+  return (
+    <div>
+      <h1>All Questions</h1>
+    </div>
+  );
+};
+
+export default Faq;
