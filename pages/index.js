896c27ff3d98088819963ae03521ac85dffaf2d8
louislam <louislam7229@gmail.com>
2022-10-25 14:36:30 -0700

    added Terminal styling and window, GifFeatures

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import TerminalWindow from '../components/TerminalWindow';
 
 export default function Home() {
   return (
@@ -13,8 +14,53 @@ export default function Home() {
         <meta name="keywords" content="reactime" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>lorem</p>
+        <h1 className={styles.title}>
+          State Management and Time Travel Debugging
+        </h1>
+        <p className={styles.text}>
+          Reactime is an open source Chrome developer tool for time travel
+          debugging and performance monitoring in React applications. Reactime
+          enables developers to record snapshots of application state, jump
+          between and inspect state snapshots, and monitor performance metrics
+          such as component render time and render frequency.
+        </p>
+        <br></br>
+        <div className="terminal-window">
+          <TerminalWindow
+            command={[
+              'git clone https://github.com/open-source-labs/reactime.git',
+              'cd reactime',
+              'npm install --force',
+              'npm run build',
+            ]}
+          >
+            <div>
+              <h3 className="terminal-header">
+                Get started easily with npm and instantly start state debugging
+                now!
+                <br />
+                <br />
+                1. Clone repo from Reactime GitHub
+                <br />
+                2. Install dependencies inside repository
+                <br />
+                3. Start Reactime's development environment
+              </h3>
+            </div>
+          </TerminalWindow>
+        </div>
+        {/* <GifFeatures />
+        <Features>
+          <h2 style={{ color: styles.reactGreen }}>
+            <strong>Additional Features</strong>
+          </h2>
+        </Features>
+
+        <Team>
+          <h2 style={{ color: styles.reactGreen }}>
+            <strong>Contributors</strong>
+          </h2>
+        </Team> */}
         <Link href={'/info'}>
           <a className={styles.btn}>See info</a>
         </Link>
