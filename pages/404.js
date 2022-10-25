20edf429defdc4f1db3e788d11434e58a186971c
louislam <louislam7229@gmail.com>
2022-10-25 00:15:09 -0700

    added 3sec redirect on 404page to home using router.push method

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, [router]);
+
   return (
     <div className="not-found">
       <h1> Oops......</h1>
