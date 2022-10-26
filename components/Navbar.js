e2ec69447754ef1c0af43533fc8395ff50822a12
louislam <louislam7229@gmail.com>
2022-10-26 00:44:00 -0700

    edited Navbar with linkedin, chrome and github

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,17 +4,12 @@ import Image from 'next/image';
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">
-      </div>
-      <Link href={'/'}>
-        <a>Home</a>
-      </Link>
-      <Link href={'/about'}>
-        <a>About</a>
-      </Link>
-      <Link href={'/info'}>
-        <a>Info</a>
-      </Link>
+      <div className="logo"></div>
+      <a href="https://www.linkedin.com/company/reactime/">LinkedIn</a>
+      <a href="https://chrome.google.com/webstore/detail/reactime/cgibknllccemdnfhfpmjhffpjfeidjga?hl=en-US">
+        Chrome
+      </a>
+      <a href="https://github.com/open-source-labs/reactime">Github</a>
     </nav>
   );
 };
