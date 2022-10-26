c8c4f638a83e33f36b92d77998a30cfbaaf9a8c9
louislam <louislam7229@gmail.com>
2022-10-25 22:01:53 -0700

    removed Footer and Navbar import from index;

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import TerminalWindow from '../components/TerminalWindow';
